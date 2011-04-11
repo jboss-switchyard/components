f32eaf245c63194d0916c9ad5adacf77a240dfb8
Daniel Bevenius <daniel.bevenius@redpill-linpro.com>
2011-04-11 20:50:15 +0200

    Adding an clojure component. Currently only a implementation.clojure is available.

---
+++ clojure/src/main/java/org/switchyard/component/clojure/config/model/v1/V1ClojureScriptModel.java
@@ -0,0 +1,53 @@
+package org.switchyard.component.clojure.config.model.v1;
+
+import javax.xml.namespace.QName;
+
+import org.switchyard.component.clojure.config.model.ClojureComponentImplementationModel;
+import org.switchyard.component.clojure.config.model.ClojureScriptModel;
+import org.switchyard.config.Configuration;
+import org.switchyard.config.model.BaseNamedModel;
+import org.switchyard.config.model.Descriptor;
+
+/**
+ * A version 1 implementation of a ScriptModel.
+ * 
+ * @author Daniel Bevenius
+ *
+ */
+public class V1ClojureScriptModel extends BaseNamedModel implements ClojureScriptModel {
+    
+    /**
+     * No-args constructor.
+     */
+    public V1ClojureScriptModel() {
+        super(new QName(ClojureComponentImplementationModel.DEFAULT_NAMESPACE, SCRIPT));
+    }
+    
+    /**
+     * Constructor.
+     * 
+     * @param config The configuration model.
+     * @param desc The descriptor for this model.
+     */
+    public V1ClojureScriptModel(Configuration config, Descriptor desc) {
+        super(config, desc);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.switchyard.component.clojure.config.model.v1.ClojureScriptModel#getScript()
+     */
+    @Override
+    public String getScript() {
+        return getModelValue();
+    }
+    
+    /* (non-Javadoc)
+     * @see org.switchyard.component.clojure.config.model.v1.ClojureScriptModel#setScript(java.lang.String)
+     */
+    @Override
+    public ClojureScriptModel setScript(final String script) {
+        setModelValue(script);
+        return this;
+    }
+
+}
