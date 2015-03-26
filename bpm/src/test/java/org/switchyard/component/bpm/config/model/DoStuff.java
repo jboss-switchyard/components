fdbb9abe6cb3f1468d52c2d405ae6cb825e5ca36
dvirgiln <david.virgil.naranjo@gmail.com>
2015-03-26 10:31:36 +0100

    SWITCHYARD-2620   Remove BRMS references

--- bpm/src/test/java/org/switchyard/component/bpm/config/model/DoStuff.java
+++
@@ -1,7 +0,0 @@
-package org.switchyard.component.bpm.config.model;
-
-public interface DoStuff {
-
-    public void process(Object stuff);
-
-}
--- rules/src/test/java/org/switchyard/component/rules/config/model/DoStuff.java
+++
@@ -1,7 +0,0 @@
-package org.switchyard.component.rules.config.model;
-
-public interface DoStuff {
-
-    public void process(Object stuff);
-
-}
